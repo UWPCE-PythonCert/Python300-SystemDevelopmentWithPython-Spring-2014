8bf4e461fd67f1170b90698451caec03e5b7724f
Joseph Sheedy <joseph.sheedy@gmail.com>
2014-04-01 16:05:40 -0700

    updates

--- week-02/debugging/examples/long_loop.py
+++ week-02/debugging/examples/long_loop.py
@@ -3,6 +3,8 @@ def long_loop():
     import ipdb;ipdb.set_trace()
     for i in range(int(1e04)):
         i+1
+        if i == 777:
+            raise Exception("terrible bug")
     result = 1 + 1
     return result
 
