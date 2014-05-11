460fec9507ce89a0af905b8ac6de0eba6e592fea
Chris Barker <Chris.Barker@noaa.gov>
2014-05-10 18:04:39 -0700

    updated C API example

--- week-08/extensions/code/c-api/add.c
+++ week-08/extensions/code/c-api/add.c
@@ -14,26 +14,16 @@ add(PyObject *self, PyObject *args)
 }
 
 // Module's method table and initialization function
-// See: http://docs.python.org/extending/extending.html#the-module-s-method-table-and-initialization-function
+// see: https://docs.python.org/2/extending/extending.html#the-module-s-method-table-and-initialization-function
 static PyMethodDef AddMethods[] = {
     {"add", add, METH_VARARGS, "add two numbers"},
-    {NULL, NULL, 0, NULL}
+    {NULL, NULL, 0, NULL} // sentinel
 };
 
-void initadd(void) {
+
+PyMODINIT_FUNC
+initadd(void) {
     // Module's initialization function
     // Will be called again if you use Python's reload()
-
-    PyImport_AddModule("add");
-    Py_InitModule("add", AddMethods);
+    (void) Py_InitModule("add", AddMethods);
 }
-
-// int main(int argc, char *argv[]) {
-//     // Py_SetProgramName(argv[0]);
-
-//     Py_Initialize();
-
-//     initadd();
-
-//     return 0;
-// }
